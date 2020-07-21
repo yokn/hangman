2aed967f818dc17471dede473c394a9e4a9c75a9
yokn <discolouredattic@gmail.com>
2020-07-21 16:55:33 +0300

    remove whitespace characters when generating word

--- game.rb
+++ game.rb
@@ -9,24 +9,21 @@ class Game
 
   def setup_game
     # resume?
-    puts generate_secret_word
+    generate_secret_word
     play_game
   end
 
   def generate_secret_word
-    @random_seed = rand(61_404)
     @words_file = File.open('5desk.txt', 'r')
-    p @random_seed
-    @random_seed.times { @words_file.readline }
-    @secret_word = @words_file.readline until @secret_word.length > 4 && @secret_word.length < 13
+    rand(61_404).times { @words_file.readline }
+    @secret_word = @words_file.readline.gsub(/\s/, '') until @secret_word.length > 4 && @secret_word.length < 13
 
-    # p @secret_word
+    p @secret_word
   end
 
   def play_game
     while @lives.positive?
       puts 'here'
-      p @secret_word
       puts 'placeholder'
       @lives -= 1
     end
