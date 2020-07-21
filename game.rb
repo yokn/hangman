5115e79004c6a9287da0cff2efdded737cff3862
yokn <discolouredattic@gmail.com>
2020-07-21 17:05:43 +0300

    add logic for getting and checking guesses

--- game.rb
+++ game.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'player'
+require_relative 'board'
+
 class Game
   def initialize
-    @guesses = []
-    @lives = 2
+    @wrong_guesses = []
+    @lives = 8
     @secret_word = ''
+    @player = Player.new
+    @board = Board.new
   end
 
   def setup_game
@@ -17,15 +22,26 @@ class Game
     @words_file = File.open('5desk.txt', 'r')
     rand(61_404).times { @words_file.readline }
     @secret_word = @words_file.readline.gsub(/\s/, '') until @secret_word.length > 4 && @secret_word.length < 13
-
     p @secret_word
+    @secret_word = @secret_word.split
+  end
+
+  def check_guess(guess)
+    @secret_word.each_with_index do |value, index|
+      if value == guess
+        reveal_char(index)
+      else
+        @lives -= 1
+        @wrong_guesses.push(guess)
+      end
+    end
   end
 
   def play_game
     while @lives.positive?
-      puts 'here'
-      puts 'placeholder'
-      @lives -= 1
+      check_guess(@player.get_guess)
+      @board.display_board
     end
+    puts 'Game Over!'
   end
 end
--- player.rb
+++ player.rb
@@ -2,4 +2,9 @@
 
 class Player
   def initialize; end
+
+  def get_guess
+    puts 'Please enter a character'
+    gets.chomp.upcase
+  end
 end
